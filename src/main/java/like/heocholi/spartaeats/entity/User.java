c3e20a4e4e2610684671e9e6f5aa60a8d5863936
jay1261 <dongjae73@naver.com>
2024-06-19 21:56:32 +0900

    feat/#3 : 회원가입 기능
    회원가입 기능 추가

--- src/main/java/like/heocholi/spartaeats/entity/User.java
+++ src/main/java/like/heocholi/spartaeats/entity/User.java
@@ -10,11 +10,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import like.heocholi.spartaeats.constants.UserRole;
+import like.heocholi.spartaeats.constants.UserStatus;
+import like.heocholi.spartaeats.dto.SignupRequestDto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "users")
+@NoArgsConstructor
 public class User extends Timestamped{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,12 +33,24 @@ public class User extends Timestamped{
 	private String address;
 	
 	private String refreshToken;
-	
+
+	@Enumerated(value = EnumType.STRING)
+	private UserStatus userStatus;
+
 	@Enumerated(value = EnumType.STRING)
 	private UserRole role;
 	
 	@OneToOne(mappedBy = "manager", fetch = FetchType.LAZY)
 	private Store store;
+
+	public User(SignupRequestDto requestDto, String encodedPassword) {
+		this.userId = requestDto.getUserId();
+		this.password = encodedPassword;
+		this.name = requestDto.getName();
+		this.address = requestDto.getAddress();
+		this.userStatus = UserStatus.ACTIVE;
+		this.role = UserRole.ROLE_CUSTOMER;
+	}
 
 	public void saveRefreshToken(String refreshToken){
 		this.refreshToken = refreshToken;
