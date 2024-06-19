b8ed70229483163516aafb724006b72ec9b96b58
llocr <36-96@naver.com>
2024-06-19 15:18:10 +0900

    add : init project

---
+++ src/main/java/like/heocholi/spartaeats/SpartaEatsApplication.java
@@ -0,0 +1,13 @@
+package like.heocholi.spartaeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaEatsApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaEatsApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/like/heocholi/spartaeats/SpartaEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package like.heocholi.spartaeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaEatsApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
