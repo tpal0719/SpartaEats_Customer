20c7c09b34fc553065822fe8fcfc848ec0181f22
llocr <36-96@naver.com>
2024-06-20 09:18:16 +0900

    add : 공통 사용 DTO 추가 & GlobalExceptionHandler 추가

---
+++ src/main/java/like/heocholi/spartaeats/dto/ResponseMessage.java
@@ -0,0 +1,14 @@
+package like.heocholi.spartaeats.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ResponseMessage<T> {
+	private Integer statusCode;
+	private String message;
+	private T data;
+}
