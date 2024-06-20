df9272e0b54b18d50fcdcb834ef4d6bcfb964f4c
jay1261 <dongjae73@naver.com>
2024-06-20 17:15:17 +0900

    feat/#3: JwtExceptionFilter 추가 및 적용
    필터에서 동작하는 exception handler 추가

--- src/main/java/like/heocholi/spartaeats/config/SecurityConfig.java
+++ src/main/java/like/heocholi/spartaeats/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package like.heocholi.spartaeats.config;
 
 import like.heocholi.spartaeats.jwt.JwtAuthenticationFilter;
 import like.heocholi.spartaeats.jwt.JwtAuthorizationFilter;
+import like.heocholi.spartaeats.jwt.JwtExceptionFilter;
 import like.heocholi.spartaeats.jwt.JwtUtil;
 import like.heocholi.spartaeats.repository.CustomerRepository;
 import like.heocholi.spartaeats.security.UserDetailsServiceImpl;
@@ -54,6 +55,11 @@ public class SecurityConfig {
     }
 
     @Bean
+    public JwtExceptionFilter jwtExceptionFilter(){
+        return new JwtExceptionFilter();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable());
 
@@ -72,6 +78,7 @@ public class SecurityConfig {
 
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtExceptionFilter(), JwtAuthorizationFilter.class);
 
         return http.build();
     }
